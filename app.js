9acc890f93847cecf1f7171085fad0c0ce2fb6a3
moriyu <moriyu@MacBook-Pro.local>
2022-07-15 14:17:20 +0900

    mod dbURL

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use("/api/v1/tasks",taskRoute);
 //データーベースと接続
 const start = async () => {
     try {
-        await connectDB(process.env.MONGO_URL);
+        await connectDB(process.env.MONGO_HEROKU_URL || process.env.MONGO_URL);
         app.listen(process.env.PORT || PORT, console.log("サーバーが接続できました"))
     } catch(err) {
         console.log(err);
